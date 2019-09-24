adb0067c29584d4616fe9650e2a856aa93efb8c8
rashier <rashier13@gmail.com>
2019-09-24 14:01:59 +0200

    done

--- configs/nodemailer.config.js
+++ configs/nodemailer.config.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer')
 module.exports = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: process.env.USER_NAME,
-    pass: process.env.PASS 
+    user: process.env.GMAIL_USERNAME,
+    pass: process.env.GMAIL_PASS 
   }
 });
