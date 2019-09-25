d5293386f326aea76889af892a229dbe3e4b228b
borgila <elbienesbueno@gmail.com>
2019-09-25 18:38:02 +0200

    recipes

--- routes/profile.js
+++ routes/profile.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const activate = require(`../middlewares/activeMid`);
 
 router.get("/userProfile", activate.checkActive, (req, res, next) => {
+
   res.render("profile/userProfile", { message: req.flash("error") });
 });
 
@@ -11,13 +12,5 @@ router.get("/usersearchRecipes", activate.checkActive, (req, res, next) => {
   res.render("profile/userSearchRecipes", { message: req.flash("error") });
 });
 
-// router.get("/recipe/:borja", (req, res) => {
-//   let recipeID = req.params.bora
 
-//   axios.get("https://www.spoonful.com/recipes/" + recipeID + "/analyzendInstruction").then(recipe => {
-//     steps = ["a", "b", "c"]
-
-//     res.render("view-recipe.hbs", recipe)
-//   })
-// })
 module.exports = router;
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,27 +3,22 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-
 router.get("/:id", (req, res) => {
   let recipeID = req.params.id;
-  axios.get(`https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=cc6d41a5219b4fcfbb7f408d71cd3a3c`)
+  axios.get(`https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=${process.env.SPOON_KEY}`)
     
     .then(recipeInfo => {
-      console.log(recipeInfo.data)
+      
       let recipeDetail=recipeInfo.data
       res.render("profile/userViewRecipe", { recipeDetail });
     })
     .catch(error => console.log(error));
 });
+router.get("/price/:id", (req,res)=>{
+  let recipeID=req.params.id;
+  axios.get(`https://api.spoonacular.com/recipes/${recipeID}/priceBreakdownWidget.json?apiKey=${process.env.SPOON_KEY}`)
+}
 
-// router.get("/recipe/:borja", (req, res) => {
-//   let recipeID = req.params.bora
-
-//   axios.get("https://www.spoonful.com/recipes/" + recipeID + "/analyzendInstruction").then(recipe => {
-//     steps = ["a", "b", "c"]
-
-//     res.render("view-recipe.hbs", recipe)
-//   })
-// })
+)
 
 module.exports = router;
