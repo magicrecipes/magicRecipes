01a68b4966e4d5fd4229cc90cdb086ece46a5cf3
borgila <elbienesbueno@gmail.com>
2019-09-25 11:42:08 +0200

    DOMRECIPE

--- public/javascripts/getRecipe.js
+++ public/javascripts/getRecipe.js
@@ -1,17 +1,36 @@
-const recipeApi = new SpoonApi(
-  "https://api.spoonacular.com/recipes"
+const recipeApi = new SpoonApi("https://api.spoonacular.com/recipes");
 
-);
-
-document.getElementById("search").addEventListener("click", e => {
-  e.preventDefault()
-  let ingredient1 = document.querySelector("#ingredient_1").value;
+document.getElementById("btn-searchRecipes").addEventListener("click", e => {
+  e.preventDefault();
+  let ingredient1 = document.querySelector("#ingredient-1").value;
   let ingredient2 = document.querySelector("#ingredient-2").value;
   let ingredient3 = document.querySelector("#ingredient-3").value;
-  recipeApi.getRecipe(ingredient1,ingredient2,ingredient3).then((allRecipes)=>
+  // let contenedor =document.querySelector("#receta").innerHTML;
+  // var ul = document.getElementById("receta");
+  // var li = document.createElement("li");
+  // li.appendChild(document.createTextNode("Four"));
+  // ul.appendChild(li);
 
-  document.querySelector("#vamos").innerHTML= allRecipes.data[0].title
-  )
+  recipeApi
+    .getRecipe(ingredient1, ingredient2, ingredient3)
+    .then(allRecipes => {
+      allRecipes.data.forEach(
+        (element) => {
+          const ul = document.getElementById("found-recipes");
+          let elRecipe = document.createElement("a");
+          elRecipe.appendChild(document.createTextNode(element.title));
+          elRecipe.href=`/auth/recipe/${element.id}`
+          ul.appendChild(elRecipe);
+          elRecipe = document.createElement("img");
+          elRecipe.src = element.image;
+          //  li.id="element.id"
+          ul.appendChild(elRecipe);
+
+          console.log(allRecipes);
+        
+        }
+       
+      );
+    });
 });
 // recipeApi.getPrice()
-
